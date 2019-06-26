3e0b07326dd99f30208d6e6cda143ff157a01a14
Florian Meyer <florian.meyer@capgemini.com>
2019-06-26 18:03:04 +0200

    Created (hopefully) working login.php

--- login.php
+++ login.php
@@ -1,5 +1,34 @@
+<?php
 const MYSQLI_IP = "localhost";
 const MYSQLI_PORT = 3306;
 const MYSQLI_USER = "test";
-const MYSQLI_PASS = "testpass";
+const MYSQLI_PASS = "test123";
 const MYSQLI_DB = "oper";
+
+$mysqli = new mysqli(MYSQLI_IP, MYSQLI_USER, MYSQLI_PASS, MYSQLI_DB, MYSQLI_PORT);
+
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$stmt = $mysqli->prepare('SELECT Hash, Salt FROM Users WHERE Username=?');
+
+$stmt->bind_param("s", $username);
+$stmt->bind_result($hash, $salt);
+
+$username = $_POST['Username'];
+
+if ($stmt->execute()){
+    if (!$stmt->fetch()) {
+		echo "User does not exist";
+		exit();
+	}
+    $hashed = hash("sha256", $_POST['Password'].$salt);
+    if ($hashed == $hash) echo "true";
+    else echo "false";
+} else {
+    header("HTTP/1.1 500 Internal Server Error");
+    echo $stmt->error;
+}
+?>
