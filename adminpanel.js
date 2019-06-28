2d38aa947ee9c73558c26804f066cd85eaadeba0
Nick Grüßner <n.gruessner@t-online.de>
2019-06-28 13:28:09 +0200

    Cleaned up

--- adminpanel.js
+++ adminpanel.js
@@ -1,9 +1,8 @@
 function getusers() {
 
-
     var baseurl = window.location.href.split('/').slice(0, window.location.href.split('/').length - 1).toString().replace(/\,/g, '/');
     var xmlHttp = new XMLHttpRequest();
-    var getusersurl = baseurl+"/getusers.php";
+    var getusersurl = baseurl + "/getusers.php";
 
     xmlHttp.open("GET", getusersurl, true); //true for asynchronous request
 
@@ -29,29 +28,19 @@ function fillarr(array) {
     for (var i = 0; i < array.length; i++) {
         var row = document.createElement("tr");
 
-        var cell = document.createElement("td");
-        var cellText = document.createTextNode(array[i].Username);
-        cell.appendChild(cellText);
-        row.appendChild(cell);
+        row.appendChild(createtd(array[i].Username));
+        row.appendChild(createtd(array[i].Mail));
+        row.appendChild(createtd(array[i].Salt));
 
-        var cell = document.createElement("td");
-        var cellText = document.createTextNode(array[i].Mail);
-        cell.appendChild(cellText);
-        row.appendChild(cell);
-
-        var cell = document.createElement("td");
-        var cellText = document.createTextNode(array[i].Salt);
-        cell.appendChild(cellText);
-        row.appendChild(cell);
-
-        // for (var j = 0; j < 3; j++) {
-        //     var cell = document.createElement("td");
-        //     var cellText = document.createTextNode(array[i][j]);
-        //     cell.appendChild(cellText);
-        //     row.appendChild(cell);
-        // }
         table.appendChild(row);
     }
+}
+
+function createtd(tdvalue) {
+    var cell = document.createElement("td");
+    var cellText = document.createTextNode(tdvalue);
+    cell.appendChild(cellText);
+    return cell;
 }
 
 window.onload = getusers;
