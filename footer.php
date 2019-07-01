e7c5a97d80481b765bd364e44cab5a4d3ff3ecea
Nick Grüßner <n.gruessner@t-online.de>
2019-07-01 13:55:54 +0200

    adjustet footer and animation timing

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer class="footer-general">
     <div class="row ml-5 mr-5">
-        <div class="col"><a id="myLink" title="Click to do something" href="PleaseEnableJavascript.html" onclick="MyFunction();return false;">link text</a></div>
         <div class="col"><a href="impressum.html">Impressum</a></div>
-        <div class="col"><a href="impressum.html">Impressum</a></div>
-        <div class="col"><a href="impressum.html">Impressum</a></div>
+        <div class="col"><a href="impressum.html#kontakt">Kontakt</a></div>
+        <div class="col"><a href="impressum.html">Datenschutzerklärung</a></div>
+        <div class="col">Deutsche Lebensrettungsgesellschaft e.V, Ortsverein Buxtehude 2019</div>
     </div>
 </footer>
 </body>
