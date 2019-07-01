b2ab56a897fe82b7cfcc0eabb52aa5d7fb4755ef
Nick Grüßner <n.gruessner@t-online.de>
2019-07-01 15:52:55 +0200

    Main page created

--- adminpanel.php
+++ adminpanel.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <title>OPER-Admin Panel</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
+    <link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="styles.css">
     <script lang="javascript" type="text/javascript" src="adminpanel.js"></script>
 
 </head>
---
+++ main.js
@@ -0,0 +1,44 @@
+window.onload = getcourses;
+var username = "testuser"; //TODO: load dynamically
+var baseurl = window.location.href.split('/').slice(0, window.location.href.split('/').length - 1).toString().replace(/\,/g, '/');
+
+function getcourses() {
+    var xmlHttp = new XMLHttpRequest();
+    var url = baseurl + "/getcourses.php?user=" + username;
+
+    xmlHttp.open("GET", url, true); //true for asynchronous request
+
+    xmlHttp.onload = function(e) {
+        if (xmlHttp.status === 200) {
+            var result = xmlHttp.responseText;
+            console.log(xmlHttp.responseText);
+            var dataset = JSON.parse(result);
+            fillarr(dataset);
+        } else {
+            console.log(xmlHttp.statusText);
+        }
+    }
+    xmlHttp.send(null);
+}
+
+function fillarr(array) {
+
+    var table = document.getElementById("usertable");
+    table.deleteRow(1);
+    var tablebody = document.getElementById("tablebody");
+    for (var i = 0; i < array.length; i++) {
+        var row = document.createElement("tr");
+        var createClickHandler =
+            function(array, i) {
+                return function() {
+                    window.location.href = baseurl + '/course?course=' + array[i].Course;
+                    //What happens, when you click on a row
+                };
+            };
+        row.onclick = createClickHandler(array, i);
+        row.appendChild(createtd(array[i].Course));
+        row.appendChild(createtd(array[i].Participants));
+
+        tablebody.appendChild(row);
+    }
+}
