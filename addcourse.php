3af06038c5b6ab4f508f5190cb63d794c3f8b8bf
Florian Meyer <florian.meyer@capgemini.com>
2019-07-08 12:57:05 +0200

    addcourse.php

---
+++ addcourse.php
@@ -0,0 +1,25 @@
+<?php
+require_once("constants.php");
+require_once("auth.php");
+
+$mysqli = new mysqli(MYSQLI_IP, MYSQLI_USER, MYSQLI_PASS, MYSQLI_DB, MYSQLI_PORT);
+
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$stmt = $mysqli->prepare('INSERT INTO Courses (Name, CreatorId) VALUES (?, ?)');
+$stmt->bind_param("si", $coursename, $userid);
+
+$coursename = $_POST['CourseName'];
+$userid = $_COOKIE['user'];
+
+if ($stmt->execute()){
+    header("Location: participants.php");
+    exit();
+} else {
+    header("HTTP/1.1 500 Internal Server Error");
+    echo $stmt->error;
+}
+?>
