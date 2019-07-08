f828fe0a2c68508d00f47c47514c71c293ccd5c9
Florian Meyer <florian.meyer@capgemini.com>
2019-07-08 12:15:43 +0200

    addparticipant.php

--- addparticipant.php
+++ addparticipant.php
@@ -1,3 +1,26 @@
 <?php
+require_once("constants.php");
+require_once("adminauth.php");
 
+$mysqli = new mysqli(MYSQLI_IP, MYSQLI_USER, MYSQLI_PASS, MYSQLI_DB, MYSQLI_PORT);
+
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$stmt = $mysqli->prepare('INSERT INTO Participants FirstName, LastName, DateOfBirth VALUES (?, ?, ?)');
+$stmt->bind_param("sss", $firstname, $lastname, $dateofbirth);
+
+$firstname = $_POST['FirstName'];
+$lastname = $_POST['LastName'];
+$dateofbirth = $_POST['DateOfBirth'];
+
+if ($stmt->execute()){
+    header("Location: participants.php");
+    exit();
+} else {
+    header("HTTP/1.1 500 Internal Server Error");
+    echo $stmt->error;
+}
 ?>
