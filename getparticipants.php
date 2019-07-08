4c911f9e5c31f83089c90652e46eaac35e2b2bf0
Florian Meyer <florian.meyer@capgemini.com>
2019-07-08 12:09:40 +0200

    getparticipants.php

---
+++ getparticipants.php
@@ -0,0 +1,20 @@
+<?php
+require_once("constants.php");
+require_once("adminauth.php");
+
+$mysqli = new mysqli(MYSQLI_IP, MYSQLI_USER, MYSQLI_PASS, MYSQLI_DB, MYSQLI_PORT);
+
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$stmt = $mysqli->query('SELECT * FROM Participants');
+
+$users = array();
+
+while ($row = $stmt->fetch_assoc()){
+    $users[] = $row;
+}
+echo json_encode($users);
+?>
