ba3c45e5bdb53750f4186686f371c97c5b1b5810
Florian Meyer <florian.meyer@capgemini.com>
2019-07-08 12:59:14 +0200

    updatecourse.php

---
+++ updatecourse.php
@@ -0,0 +1,23 @@
+<?php
+require_once("constants.php");
+require_once("auth.php");
+
+$mysqli = new mysqli(MYSQLI_IP, MYSQLI_USER, MYSQLI_PASS, MYSQLI_DB, MYSQLI_PORT);
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+$stmt = $mysqli->prepare("UPDATE Course SET Name=? WHERE Id=?");
+$stmt->bind_param("si", $coursename, $courseid);
+$coursename = $_POST['CourseName'];
+$courseid = $_POST['CourseId'];
+if ($stmt->execute()){
+    $count = $stmt->affected_rows;
+    
+    header('Location: participants.php?message=Successfully%20updated%20'.$count.'%20entries.');
+    exit();
+} else {
+    header("HTTP/1.1 500 Internal Server Error");
+    echo $stmt->error;
+}
+?> 
