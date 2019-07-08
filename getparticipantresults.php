a8abaee379680b7d7c8e4b098b63b3c45f43db6d
Florian Meyer <florian.meyer@capgemini.com>
2019-07-08 12:40:22 +0200

    getparticipantresults.php

---
+++ getparticipantresults.php
@@ -0,0 +1,22 @@
+<?php
+require_once("constants.php");
+require_once("auth.php");
+
+$mysqli = new mysqli(MYSQLI_IP, MYSQLI_USER, MYSQLI_PASS, MYSQLI_DB, MYSQLI_PORT);
+
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$stmt = $mysqli->query('SELECT courses.Name AS CourseName, Grade FROM courseparticipants '
+.'INNER JOIN courses ON courses.Id=courseparticipants.CourseId INNER JOIN participants ON participants.Id=courseparticipants.ParticipantId WHERE ParticipantId=?');
+$stmt->bind_param("i", $_POST['ParticipantId']);
+
+$users = array();
+
+while ($row = $stmt->fetch_assoc()){
+    $users[] = $row;
+}
+echo json_encode($users);
+?>
