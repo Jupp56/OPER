bc387644df582cdfeaa0e41ce7c7cc6baaed14b1
Nick Grüßner <n.gruessner@t-online.de>
2019-07-08 13:24:53 +0200

    cookie changed

--- userdata.php
+++ userdata.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-auto myborder">
             <form action="logout.php">
-            Benutzer: <?php print($_COOKIE['user']); ?><br>
+            Benutzer: <?php print($_COOKIE['username']); ?><br>
             <input class="hidden" name="username" value="<?php print($_COOKIE['user']); ?>">
             <button class="mt-1 float-right"type="submit">Abmelden</button>
             </form>
