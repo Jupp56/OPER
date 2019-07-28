db6cc39cf79059e04497e7b2001c3ee8726bfdcf
Nick Grüßner <n.gruessner@t-online.de>
2019-07-28 13:05:52 +0200

    better layout?

--- course.js
+++ course.js
@@ -36,8 +36,8 @@ function fillarr(array) {
         row.appendChild(createtd(array[i].LastName));
         row.appendChild(createtd(array[i].DateOfBirth));
         row.appendChild(createinput(array[i].Grade));
-        row.appendChild(createbutton("X", "button-warn", deleteaccountgrade, array[i].RelationId));
         row.appendChild(createtd(array[i].RelationId, false, "hidden"));
+        row.appendChild(createbutton("X", "button-warn", deleteaccountgrade, array[i].RelationId));
         tablebody.appendChild(row);
     }
 }
