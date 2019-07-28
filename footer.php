66efaf475a2c40e84524bbb9421696cfd4e4674c
Nick Grüßner <n.gruessner@t-online.de>
2019-07-28 12:58:41 +0200

    better footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="row ml-5 mr-5">
         <div class="col"><a href="impressum.html">Impressum</a></div>
         <div class="col"><a href="impressum.html#kontakt">Kontakt</a></div>
-        <div class="col">Icons made by <a href="https://www.flaticon.com/authors/gregor-cresnar" title="Gregor Cresnar">Gregor Cresnar</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+       
         <div class="col"><a href="impressum.html">Datenschutzerklärung</a></div>
         <div class="col-auto">&#169; Deutsche Lebensrettungsgesellschaft e.V, Ortsverein Buxtehude 2019</div>
     </div>
